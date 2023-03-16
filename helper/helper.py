1acbf0043c8211107f6d8655db70a0a3cb9309b8
MFRSiam <mfrforhad1@gmail.com>
2023-03-16 21:49:33 +0600

    Tokenizing The Data

--- helper/helper.py
+++ helper/helper.py
@@ -1,9 +1,9 @@
 
 #open The Files
 
+import re
 
-
-def ReadAFileAndDevideBySectence(filename : str):
+def ReadAFileAndDevideByReview(filename : str):
     # read the file and split it into sections baseon the review
     with open(filename, 'r') as f:
         text = f.read()
@@ -13,3 +13,32 @@ def ReadAFileAndDevideBySectence(filename : str):
     # for section in sections:
     #     section.strip()
     return sections
+
+def DevideBySentence(data : list):
+    sentence_data = []
+    
+    for x in data:
+        sentence = x.split('##')
+        sentence_data.append(sentence)
+    return sentence_data
+
+def ConvertListOfReviewOfListOfSentenceToListOfSentence(data:list):
+    ret_data = [item for sublist in data for item in sublist]
+    return ret_data
+
+
+def Remove_Feature(sentences : list):
+    cleaned_sentences = []
+    for sentence in sentences:
+        cleaned_sentence = re.sub(r'\b\w+\[[+-]?\d+\]', '', sentence)
+        cleaned_sentences.append(cleaned_sentence)
+    return cleaned_sentences
+
+def Remove_Annotations(sentences: list):
+    cleaned_sentences  = []
+    pattern = r'\[(?:u|p|s|cc|cs)\]|\[/(?:u|p|s|cc|cs)\]'
+    
+    for sentence in sentences:
+        cleaned_sentence = re.sub(pattern, '', sentence)
+        cleaned_sentences.append(cleaned_sentence)
+    return cleaned_sentences
--- main.py
+++ main.py
@@ -1,15 +1,46 @@
+import nltk
 from helper.helper import *
+from nltk.corpus import stopwords
 
 # Read The Data
-data_1 = ReadAFileAndDevideBySectence("data/Apex AD2600 Progressive-scan DVD player.txt")
-data_2 = ReadAFileAndDevideBySectence("data/Canon G3.txt")
-data_3 = ReadAFileAndDevideBySectence("data/Creative Labs Nomad Jukebox Zen Xtra 40GB.txt")
-data_4 = ReadAFileAndDevideBySectence("data/Nikon coolpix 4300.txt")
-data_5 = ReadAFileAndDevideBySectence("data/Nokia 6610.txt")
+data_1 = ReadAFileAndDevideByReview("data/Apex AD2600 Progressive-scan DVD player.txt")
+data_2 = ReadAFileAndDevideByReview("data/Canon G3.txt")
+data_3 = ReadAFileAndDevideByReview("data/Creative Labs Nomad Jukebox Zen Xtra 40GB.txt")
+data_4 = ReadAFileAndDevideByReview("data/Nikon coolpix 4300.txt")
+data_5 = ReadAFileAndDevideByReview("data/Nokia 6610.txt")
 
 
 # Join Them Togeather
 data = data_1 + data_2 + data_3 + data_4 + data_5
 
-print(len(data))
+# print(len(data))
 
+temp_data = DevideBySentence(data)
+# Convert a List of List into a singluar List
+sentence_data = ConvertListOfReviewOfListOfSentenceToListOfSentence(temp_data)
+# Remove Annotated Features in the data
+sentence_data = Remove_Feature(sentence_data)
+# Remove Annotations
+sentence_data = Remove_Annotations(sentence_data)
+
+# print(sentence_data)
+
+
+# Cleaning The Data: 1: Get the stopwords From The NLTK Server
+nltk.download('stopwords')
+# 2: Set a Variable For Stop words
+stop_words = set(stopwords.words('english'))
+
+# 3: Remove The Stopwords
+data_without_stopwords = []
+for sentence in sentence_data:
+    # split the sentence into words
+    words = sentence.split()
+    # remove the stopwords
+    filtered_words = [word for word in words if word.lower() not in stop_words]
+    # join the words back into a sentence
+    filtered_sentence = ' '.join(filtered_words)
+    data_without_stopwords.append(filtered_sentence)
+
+for i in data_without_stopwords:
+    print(i)
