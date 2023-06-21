f98119ea12e5d9a6f13f3e7cf94c8cac2564143e
MFRSiam <mfrforhad1@gmail.com>
2023-06-22 02:00:50 +0600

    More Shit for narmeen to work with

--- webpage/sentiment_analysis/models.py
+++ webpage/sentiment_analysis/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class History(models.Model):
+    sentence = models.CharField(max_length=100)
+    aspect = models.CharField(max_length=50)
+    sentiment = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return self.sentence + " Sentiment: " + self.sentiment + " Aspect: " + self.aspect
--- webpage/sentiment_analysis/views.py
+++ webpage/sentiment_analysis/views.py
@@ -3,13 +3,15 @@ from flair.data import Sentence
 from flair.nn import Classifier
 from .forms import Data
 from .Trial import get_aspect_eval
+from .models import History
 # Create your views here.
 
 
 def HomePageView(request):
     if request.method == 'GET':
+        history = History.objects.all()
         form = Data()
-        return render(request,'index.html',{'form':form})
+        return render(request,'index.html',{'form':form,'history':history})
     else:
         form = Data(request.POST)
         if form.is_valid():
@@ -24,11 +26,18 @@ def HomePageView(request):
             sentiment_value = sentence.labels[0].value
             junk,aspect = get_aspect_eval(subject)
             new_form = Data()
+            
+            new_history = History(sentence = subject , sentiment= sentiment_value, aspect = aspect )
+            new_history.save()
+            
+            all_history = History.objects.all().order_by('-id')[:5]
+            
             cont = {
                 'sentiment': sentiment_value,
                 'show' : True,
                 'aspect': aspect,
-                'form' : new_form
+                'form' : new_form,
+                'history': all_history
             }
             
             return render(request,'index.html',context=cont)
