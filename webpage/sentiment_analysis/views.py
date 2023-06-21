eca319c0e5196cf1ae1a20511ff8ec3ec0747657
MFRSiam <mfrforhad1@gmail.com>
2023-06-22 04:28:23 +0600

    Added Gibrish Detector

--- webpage/sentiment_analysis/views.py
+++ webpage/sentiment_analysis/views.py
@@ -4,18 +4,35 @@ from flair.nn import Classifier
 from .forms import Data
 from .Trial import get_aspect_eval
 from .models import History
+from gibberish_detector import detector
 # Create your views here.
 
 
 def HomePageView(request):
     if request.method == 'GET':
-        history = History.objects.all()
+        history = History.objects.all().order_by('-id')[:5]
         form = Data()
         return render(request,'index.html',{'form':form,'history':history})
     else:
         form = Data(request.POST)
         if form.is_valid():
             subject = form.cleaned_data['sentence']
+            Detector = detector.create_from_model('big.model')
+            
+            new_form = Data()
+            
+            #IF Garbage then meme
+            print(Detector.is_gibberish(subject))
+            if Detector.is_gibberish(subject):
+                history = History.objects.all().order_by('-id')[:5]
+                ftxt = {
+                    'garbage':True,
+                    'form' : new_form,
+                    'history': history
+                }
+                return render(request,'index.html',context=ftxt)
+            
+            
             sentence = Sentence(subject)
             # load the NER tagger
             tagger = Classifier.load('sentiment')
@@ -25,7 +42,7 @@ def HomePageView(request):
             
             sentiment_value = sentence.labels[0].value
             junk,aspect = get_aspect_eval(subject)
-            new_form = Data()
+            
             
             new_history = History(sentence = subject , sentiment= sentiment_value, aspect = aspect )
             new_history.save()
